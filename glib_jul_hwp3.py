a7fab565b61ed77bff023b8de1e6db88980eea58
JETGLIB <153624597+JETGLIB@users.noreply.github.com>
2023-12-26 17:23:00 +0200

    Add files via upload

---
+++ glib_jul_hwp3.py
@@ -0,0 +1,43 @@
+class Discount:
+    def discount(self):
+        pass
+
+class RegularDiscount(Discount):
+    def discount(self, total_price):
+        return total_price
+
+class SilverDiscount(Discount):
+    def discount(self, total_price):
+        return f'{total_price * 0.95:.2f}'
+
+class GoldDiscount(Discount):
+    def discount(self, total_price):
+        return f'{total_price * 0.85:.2f}'
+
+class Client:
+    def __init__(self, name, discount):
+        self.name = name
+        self.discount = discount
+
+    def get_total_price(self, order):
+        total_price = sum(item.price for item in order)
+        discounted_price = self.discount.discount(total_price)
+        return discounted_price
+
+
+class Dish:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+order = [Dish("Вареники", 100), Dish("Картопля", 50), Dish("Тістечко", 80)]
+client_regular = Client("Юлія", RegularDiscount())
+total_price_regular = client_regular.get_total_price(order)
+print(f"Вартість замовлення для клієнта {client_regular.name}: {total_price_regular}")
+client_silver = Client("Лев", SilverDiscount())
+total_price_silver = client_silver.get_total_price(order)
+print(f"Вартість замовлення для клієнта {client_silver.name}: {total_price_silver}")
+client_gold = Client("Ельза", GoldDiscount())
+total_price_gold = client_gold.get_total_price(order)
+print(f"Вартість замовлення для клієнта {client_gold.name}: {total_price_gold}")
